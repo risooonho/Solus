d7bbc38718e4e14993c8a46b7cbc6d1c6c32cabc
Andre Wolff <andre.r.wolff@gmail.com>
2017-04-10 22:17:46 -0600

    Add \dist folder to repo for releases

--- solus/app.py
+++
@@ -1,9 +0,0 @@
-"""Run the game."""
-import pygame
-from classes import Game
-
-if __name__ == '__main__':
-    pygame.init()
-    screen = pygame.display.set_mode((400, 400))
-    menu_items = ['Start', 'Quit']
-    Game().title_screen(screen, menu_items)
